90c4f332ab2b3cba3d4b429c8a8ad8a0ea1d3741
Habib <habiburahmanpayenda@gmail.com>
2023-05-12 13:08:46 +0430

    Changes update method in agents controller

--- app/controllers/api/v1/agents_controller.rb
+++ app/controllers/api/v1/agents_controller.rb
@@ -5,7 +5,7 @@ module Api
     class AgentsController < ApplicationController
       before_action :authorize_admin
       def index
-        result = Agent.includes(:addresses, :contact, :properties, :property_managers).all
+        result = Agent.includes(:addresses, :contact, :properties, :property_managers).order(:created_at).all
         render json: { status: 'success', agents: result },
                include: %w[addresses contact properties property_managers]
       end
@@ -47,24 +47,46 @@ module Api
         agent_address.address_id = address.id
 
         if agent_address.save
-          result = Agent.includes(:agent_contacts, :agent_addresses,
+          result = Agent.includes(:agent_contact, :agent_addresses,
                                   :property_managers, :properties).find(agent.id)
         end
 
         if result.present?
           render json: { status: 'success', agent: result },
-                 include: %w[agent_contacts agent_addresses property_managers properties]
+                 include: %w[agent_contact agent_addresses property_managers properties]
         end
       rescue StandardError
         render json: { status: 'failed', info: 'check your data' }
       end
 
       def update
-        result = Agent.find(params[:id])
-        if result.update(agent_params)
-          render json: { status: 'success',
-                         agent: result }
+        agent = Agent.find(params[:id])
+        if agent.valid?
+          agent.name = params[:name]
+          agent.hire_date = params[:hire_date]
+          agent.status = params[:status]
+          agent.admin_id = params[:admin_id]
         end
+        address = Address.find(agent.addresses[0].id) if agent.save
+        if address.valid?
+          address.province = params[:province]
+          address.city = params[:city]
+          address.district = params[:district]
+        end
+
+        contact = Contact.find(agent.contact.id) if address.save
+
+        if contact.valid?
+          contact.phone_number_one = params[:phone_number_one]
+          contact.email_one = params[:email_one]
+        end
+        result = Agent.includes(:addresses, :contact, :properties, :property_managers).find(params[:id]) if contact.save
+        if result.present?
+          render json: { status: 'success', agent: result },
+                 include: %w[addresses contact properties
+                             property_managers]
+        end
+
       rescue StandardError
         render json: { status: 'failed', info: 'check your data' }
       end
