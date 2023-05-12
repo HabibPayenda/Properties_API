1f9352879ad5389255fd4fe39c28577b612c0071
Habib <habiburahmanpayenda@gmail.com>
2023-05-12 10:09:15 +0430

    Adds through relations to agents model

--- app/models/agent.rb
+++ app/models/agent.rb
@@ -5,5 +5,7 @@ class Agent < ApplicationRecord
   has_many :property_managers
   belongs_to :admin
   has_many :agent_addresses
+  has_many :addresses, through: :agent_addresses
   has_many :agent_contacts
+  has_many :contacts, through: :agent_contacts
 end
