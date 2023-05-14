5becb855e2a6eb822bc70752eed2cfb6e213b612
Habib <habiburahmanpayenda@gmail.com>
2023-05-14 17:25:33 +0430

    Adds image attachment to agent and creates image_url after creation

--- app/models/agent.rb
+++ app/models/agent.rb
@@ -8,4 +8,16 @@ class Agent < ApplicationRecord
   has_many :addresses, through: :agent_addresses
   has_one :agent_contact
   has_one :contact, through: :agent_contact
+  has_one_attached :image
+
+  after_create_commit do
+    set_image_url
+  end
+
+  private
+
+  def set_image_url
+    self.image_url = self.image.url
+    self.save
+  end
 end
--- app/models/property.rb
+++ app/models/property.rb
@@ -13,17 +13,8 @@ class Property < ApplicationRecord
 
   has_one_attached :image
   after_create_commit do
-    puts "**************************************"
-    puts "*************************************"
     set_image_url
   end
-
-  # def image_url
-  #   Rails.application.routes.url_helpers.url_for(image) if image.attached?
-  # end
-
-
-
 
   private
 
