e3052af646549d281d7c623870ea9c402ad5825b
Habib <habiburahmanpayenda@gmail.com>
2023-05-18 16:16:57 +0430

    Adds agent and property managers refs to lands

---
+++ db/migrate/20230518113623_add_agent_ref_to_lands.rb
@@ -0,0 +1,6 @@
+class AddAgentRefToLands < ActiveRecord::Migration[7.0]
+  def change
+    add_reference :lands, :agent, null: false, foreign_key: true
+    add_reference :lands, :property_manager, null: false, foreign_key: true
+  end
+end
