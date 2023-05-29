29634ec35658f88b943c228f2c70e8cf93ba6aac
Habib <habiburahmanpayenda@gmail.com>
2023-05-29 17:24:26 +0430

    Adds deal_info restrictions amenities and address relations to home

--- app/models/home.rb
+++ app/models/home.rb
@@ -5,4 +5,8 @@ class Home < ApplicationRecord
   has_many :home_rooms, dependent: :destroy
   has_one :home_offer
   has_one :offer, through: :home_offer
+  has_one :address, through: :property
+  has_many :amenities, through: :property
+  has_many :restrictions, through: :property
+  has_one :deal_info, through: :property
 end
