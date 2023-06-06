8f478f3da1e224ff6ce928048fd2906624953508
Habib <habiburahmanpayenda@gmail.com>
2023-06-06 15:34:00 +0430

    Sends favorited home back in response in create action

--- app/controllers/api/v1/user_favorite_homes_controller.rb
+++ app/controllers/api/v1/user_favorite_homes_controller.rb
@@ -5,11 +5,12 @@ module Api
     class UserFavoriteHomesController < ApplicationController
       def create
         user_favorite_home = UserFavoriteHome.new(user_favorite_home_params)
-        render json: { status: 'success', message: 'Entry created successfully' } if user_favorite_home.save
+        result = Home.find(user_favorite_home.home_id) if user_favorite_home.save
+        render json: { status: 'success', home: result } if result.present?
       end
 
       def destroy
-        user_favorite_home = UserFavoriteHome.find(params[:id])
+        user_favorite_home = UserFavoriteHome.where('home_id': params[:id]).first
         render json: {status: 'success', message: 'Favorite removed'} if user_favorite_home.destroy
       end
 
