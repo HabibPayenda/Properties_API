d8c866a63f519a8913e5d9801ebec364a5766585
Habib <habiburahmanpayenda@gmail.com>
2023-06-12 19:57:42 +0430

    Adds dependent destroy to Home relations

--- app/models/home.rb
+++ app/models/home.rb
@@ -3,10 +3,10 @@
 class Home < ApplicationRecord
   belongs_to :property, dependent: :destroy
   has_many :home_rooms, dependent: :destroy
-  has_one :home_offer
-  has_one :offer, through: :home_offer
-  has_one :address, through: :property
-  has_many :amenities, through: :property
-  has_many :restrictions, through: :property
-  has_one :deal_info, through: :property
+  has_one :home_offer, dependent: :destroy
+  has_one :offer, through: :home_offer, dependent: :destroy
+  has_one :address, through: :property, dependent: :destroy
+  has_many :amenities, through: :property, dependent: :destroy
+  has_many :restrictions, through: :property, dependent: :destroy
+  has_one :deal_info, through: :property, dependent: :destroy
 end
