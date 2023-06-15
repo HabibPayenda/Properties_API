020447aee45bcc538443bdb30ce537401d0af954
Habib <habiburahmanpayenda@gmail.com>
2023-06-15 10:59:29 +0430

    Adds create action to user_car_views controller

--- app/controllers/api/v1/user_car_views_controller.rb
+++ app/controllers/api/v1/user_car_views_controller.rb
@@ -3,6 +3,16 @@
 module Api
   module V1
     class UserCarViewsController < ApplicationController
+      def create
+        user_car_view = UserCarView.new(user_car_view_params)
+        render json: {status: 'success', user_car_view} if user_car_view.save
+
+        private
+
+        def user_car_view_params
+          params.require(:user_car_view).permit(:user_id, car_id)
+        end
+      end
     end
   end
 end
